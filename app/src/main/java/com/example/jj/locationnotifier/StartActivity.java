28e4b093653d56d03add0a79bbb30fd96bd722ff
Spencer Morris <spencermorris22@gmail.com>
2015-07-08 22:28:40 -0500

    Made contact into array

--- app/src/main/java/com/example/jj/locationnotifier/StartActivity.java
+++ app/src/main/java/com/example/jj/locationnotifier/StartActivity.java
@@ -14,13 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class StartActivity extends ActionBarActivity {
 
-    private Button start_button;
     private TextView dialogTextView;
     final Context context = this;
-
+    private int increment = 0;
+    private String contactInputValue;
+    public ArrayList<String> contactNumbers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,31 +56,57 @@ public class StartActivity extends ActionBarActivity {
                 alertD.show();
             }
 
-              public void onContactButtonClick(View v){
-                  startActivity(new Intent(this, GetContactNumberActivity.class));
-            }
+        public void onContactButtonClick(View view){
+            dialogTextView = (TextView) findViewById(R.id.dialogTextResults);
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View promptView = layoutInflater.inflate(R.layout.numberprompt, null);
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setView(promptView);
+
+            final EditText contactInput = (EditText) promptView.findViewById(R.id.dialogInput);
+
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //dialogTextView.setText(input.getText());
+                            contactNumbers.add(contactInput.getText().toString());
+                            dialogTextView.setText(contactNumbers.get(increment));
+                            increment += 1;
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertD = alertDialogBuilder.create();
+                            alertD.show();
 
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+
+                        @Override
+                        public boolean onCreateOptionsMenu(Menu menu) {
+                            // Inflate the menu; this adds items to the action bar if it is present.
+                            getMenuInflater().inflate(R.menu.menu_main, menu);
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onOptionsItemSelected(MenuItem item) {
+                            // Handle action bar item clicks here. The action bar will
+                            // automatically handle clicks on the Home/Up button, so long
+                            // as you specify a parent activity in AndroidManifest.xml.
+                            int id = item.getItemId();
+
+                            //noinspection SimplifiableIfStatement
+                            if (id == R.id.action_settings) {
+                                return true;
+                            }
+
+                            return super.onOptionsItemSelected(item);
+                        }
+                    }
