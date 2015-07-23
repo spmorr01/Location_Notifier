958831e2fa3917c0833276c4a48a81d1a9935a16
Spencer Morris <spencermorris22@gmail.com>
2015-07-22 23:15:16 -0500

    Added Text Features

--- app/src/main/java/com/example/jj/locationnotifier/StartActivity.java
+++ app/src/main/java/com/example/jj/locationnotifier/StartActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.gsm.SmsManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 
@@ -24,11 +29,19 @@ public class StartActivity extends ActionBarActivity {
     private int increment = 0;
     private String contactInputValue;
     public ArrayList<String> contactNumbers = new ArrayList<String>();
-
+    public int textViewCount = 5;
+    TextView[] textViewArray = new TextView[textViewCount];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);}
+        setContentView(R.layout.activity_main);
+        ReadArrayListFromSD(null, "LocationNotifier_Data");
+        dialogTextView = (TextView) findViewById(R.id.dialogTextResults);
+        for (int i = 0; i < increment; i++){
+            dialogTextView.setText(contactNumbers.get(increment));
+        }
+
+        }
 
 
         public void onDialogButtonClick(View view) {
@@ -44,7 +57,11 @@ public class StartActivity extends ActionBarActivity {
                         .setCancelable(false)
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                dialogTextView.setText(input.getText());
+                                //dialogTextView.setText(input.getText());
+                                contactNumbers.add(input.getText().toString());
+                                //textViewArray[increment].setText(contactNumbers.get(increment));
+                                dialogTextView.setText(contactNumbers.get(increment));
+                                increment += 1;
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -72,7 +89,9 @@ public class StartActivity extends ActionBarActivity {
                             //dialogTextView.setText(input.getText());
                             contactNumbers.add(contactInput.getText().toString());
                             dialogTextView.setText(contactNumbers.get(increment));
+                            //textViewArray[increment].setText(contactNumbers.get(increment));
                             increment += 1;
+                            SaveArrayListToSD(null, "LocationNotifier_Data", contactNumbers);
                         }
                     })
                     .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -86,6 +105,50 @@ public class StartActivity extends ActionBarActivity {
 
 
         }
+
+        public void onStartButtonClick(View view){
+            sendSMS(contactNumbers.get(0), "This will tell you what location I am at");
+        }
+
+        private void sendSMS(String phoneNumber, String message)
+        {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(phoneNumber, null, message, null, null);
+        }
+
+    public static <E> void SaveArrayListToSD(Context mContext, String filename, ArrayList<E> list){
+        try {
+
+            FileOutputStream fos = mContext.openFileOutput(filename + ".dat", mContext.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(list);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object ReadArrayListFromSD(Context mContext,String filename){
+        try {
+            FileInputStream fis = mContext.openFileInput(filename + ".dat");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Object obj= (Object) ois.readObject();
+            fis.close();
+            return obj;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<Object>();
+        }
+    }
+
+
+
+
+
+
+
+
 
 
                         @Override
